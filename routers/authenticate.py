eef02b979612e93d51cb12905aa7b57f3e7cc1ff
Andrei Visan <andrei.m.visan@gmail.com>
2024-07-10 18:26:08 +0200

    Added register form - just the front-end

--- routers/authenticate.py
+++ routers/authenticate.py
@@ -4,10 +4,26 @@ from . import templates
 
 router = APIRouter()
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/authenticate", response_class=HTMLResponse)
 def home(request: Request):
     context = {
             "request": request
     }
     response = templates.TemplateResponse("/authentication/pages/authenticate.html", context)
+    return response
+
+@router.get("/login", response_class=HTMLResponse)
+def home(request: Request):
+    context = {
+            "request": request
+    }
+    response = templates.TemplateResponse("/authentication/fragments/login_form.html", context)
+    return response
+
+@router.get("/register", response_class=HTMLResponse)
+def home(request: Request):
+    context = {
+            "request": request
+    }
+    response = templates.TemplateResponse("/authentication/fragments/register_form.html", context)
     return response
