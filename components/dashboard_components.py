81d744b21ce862456d7bcc48235aa8e29952bfe7
andreivisan <andrei.m.visan@gmail.com>
2024-08-11 13:06:18 +0200

    Added JSON data for Card Component

--- components/dashboard_components.py
+++ components/dashboard_components.py
@@ -14,33 +14,37 @@ async def dashboard_data_card(request: Request, total_page_views: int, progress:
                 "change": {
                     "percentage": -18,
                     "direction": "down"
-                    }
-                },
+                    },
+                "icon": "/img/svg/eye_icon.svg"
+            },
             {
                 "title": "Total Revenue",
                 "value": 51066.67,
                 "change": {
                     "percentage": 0,
                     "direction": "none"
-                    }
-                },
+                    },
+                "icon": "/img/svg/cart_icon.svg"
+            },
             {
                 "title": "Total Paying Users",
                 "value": 345,
                 "change": {
                     "percentage": -1,
                     "direction": "down"
-                    }
-                },
+                    },
+                "icon": "/img/svg/bag_icon.svg"
+            },
             {
                 "title": "Total Signups",
                 "value": 10516,
                 "change": {
                     "percentage": 15,
                     "direction": "up"
-                    }
-                }
-            ]
+                    },
+                "icon": "/img/svg/users_icon.svg"
+            }
+    ]
 
     context = {
             "request": request,
