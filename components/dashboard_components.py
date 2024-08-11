1da1acf63f4853a595d61c37c03170b7dbcc4017
andreivisan <andrei.m.visan@gmail.com>
2024-08-11 17:40:42 +0200

    Added JSON data for Graph Component

--- components/dashboard_components.py
+++ components/dashboard_components.py
@@ -6,7 +6,7 @@ from . import templates
 router = APIRouter()
 
 @router.get("/dasboard/components/data_card", response_class=HTMLResponse)
-async def dashboard_data_card(request: Request, total_page_views: int, progress: int):
+async def dashboard_data_card(request: Request):
     card_data = [
             {
                 "title": "Total page views",
@@ -51,5 +51,31 @@ async def dashboard_data_card(request: Request, total_page_views: int, progress:
             "cards": card_data
     }
     response = templates.TemplateResponse("/dashboard/components/dashboard_data_card.html", context)
+    return response
+
+@router.get("/dashboard/components/graph", response_class=HTMLResponse) 
+async def dashboard_data_graph(request: Request):
+    graph_data = {
+            "caption": "Single Dataset Table",
+            "data": [
+                { "month": "Jan", "progress": 30, "start": 0.0, "end": 0.3 },
+                { "month": "Feb", "progress": 50, "start": 0.3, "end": 0.5 },
+                { "month": "Mar", "progress": 80, "start": 0.5, "end": 0.8 },
+                { "month": "Apr", "progress": 100, "start": 0.8, "end": 1.0 },
+                { "month": "May", "progress": 65, "start": 1.0, "end": 0.65 },
+                { "month": "Jun", "progress": 45, "start": 0.65, "end": 0.45 },
+                { "month": "Jul", "progress": 15, "start": 0.45, "end": 0.15 },
+                { "month": "Aug", "progress": 32, "start": 0.15, "end": 0.32 },
+                { "month": "Sep", "progress": 60, "start": 0.32, "end": 0.6 },
+                { "month": "Oct", "progress": 90, "start": 0.6, "end": 0.9 },
+                { "month": "Nov", "progress": 55, "start": 0.9, "end": 0.55 },
+                { "month": "Dec", "progress": 40, "start": 0.55, "end": 0.4 }
+                ]
+    }
+    context = {
+        "request": request,
+        "graph_data": graph_data
+    }
+    response = templates.TemplateResponse("/dashboard/components/dashboard_graph.html", context)
     return response
 
