6d6e952be82eba7d96cb5836b53e423a2f5d0ab9
andreivisan <andrei.m.visan@gmail.com>
2024-08-11 12:24:02 +0200

    Added JSON data for Card Component

--- components/dashboard_components.py
+++ components/dashboard_components.py
@@ -7,10 +7,44 @@ router = APIRouter()
 
 @router.get("/dasboard/components/data_card", response_class=HTMLResponse)
 async def dashboard_data_card(request: Request, total_page_views: int, progress: int):
+    card_data = [
+            {
+                "title": "Total page views",
+                "value": 72245,
+                "change": {
+                    "percentage": -18,
+                    "direction": "down"
+                    }
+                },
+            {
+                "title": "Total Revenue",
+                "value": 51066.67,
+                "change": {
+                    "percentage": 0,
+                    "direction": "none"
+                    }
+                },
+            {
+                "title": "Total Paying Users",
+                "value": 345,
+                "change": {
+                    "percentage": -1,
+                    "direction": "down"
+                    }
+                },
+            {
+                "title": "Total Signups",
+                "value": 10516,
+                "change": {
+                    "percentage": 15,
+                    "direction": "up"
+                    }
+                }
+            ]
+
     context = {
             "request": request,
-            "total_page_views": total_page_views,
-            "progress": progress
+            "cards": card_data
     }
     response = templates.TemplateResponse("/dashboard/components/dashboard_data_card.html", context)
     return response
