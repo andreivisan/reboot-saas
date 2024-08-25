d2f68ac02f39d674763393ed9c1a00d687b080ff
Andrei Visan <andrei.m.visan@gmail.com>
2024-08-25 17:57:35 +0200

    Started supabase auth

--- routers/authenticate.py
+++ routers/authenticate.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter, Depends, Request, Response
+import os
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, Form, Request, Response
 from fastapi.responses import HTMLResponse
+from supabase import create_client, Client
 from . import templates
+from models.authentication.user import User
 
 router = APIRouter()
+load_dotenv()
 
-@router.get("/authenticate", response_class=HTMLResponse)
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+supabase = create_client(supabase_url, supabase_key)
+
+@router.get("/authentication_page", response_class=HTMLResponse)
 def home(request: Request):
     context = {
             "request": request
@@ -12,18 +21,22 @@ def home(request: Request):
     response = templates.TemplateResponse("/authentication/pages/authenticate.html", context)
     return response
 
-@router.get("/login", response_class=HTMLResponse)
-def home(request: Request):
+@router.get("/login_form", response_class=HTMLResponse)
+def render_login_form(request: Request):
     context = {
             "request": request
     }
     response = templates.TemplateResponse("/authentication/fragments/login_form.html", context)
     return response
 
-@router.get("/register", response_class=HTMLResponse)
-def home(request: Request):
+@router.get("/register_form", response_class=HTMLResponse)
+def render_register_form(request: Request):
     context = {
             "request": request
     }
     response = templates.TemplateResponse("/authentication/fragments/register_form.html", context)
     return response
+
+@router.post("/register", response_class=HTMLResponse)
+def register(request: Request, email: str = Form(...), password: str = Form(...)):
+    print(email + password)
