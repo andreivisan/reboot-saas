679140608eeb456edf9fd2313b9908ebc7ef2efe
Andrei Visan <andrei.m.visan@gmail.com>
2024-09-18 10:26:49 +0200

    get current user middleware

--- routers/authenticate.py
+++ routers/authenticate.py
@@ -1,9 +1,8 @@
 import os
 import json
-import bcrypt
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, Form, Request, Response
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from gotrue.errors import AuthApiError
 from supabase import create_client, Client
 from . import templates
@@ -15,14 +14,6 @@ load_dotenv()
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
 supabase = create_client(supabase_url, supabase_key)
-
-def _hashpass(pwd: str):
-    salt = bcrypt.gensalt()
-    hashed_password =  bcrypt.hashpw(pwd.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
-
-def _verifypass(plain_pwd: str, hashed_pwd: str):
-    pass
 
 @router.get("/authentication_page", response_class=HTMLResponse)
 def home(request: Request):
@@ -48,10 +39,8 @@ def render_register_form(request: Request):
     response = templates.TemplateResponse("/authentication/fragments/register_form.html", context)
     return response
 
-@router.post("/register", response_class=HTMLResponse)
+@router.post("/register")
 def register(request: Request, email: str = Form(...), password: str = Form(...)):
-    pwd = __hashpass__(password)
-    print(pwd)
     try:
         # supabase signup without email confirmation - disabled in Supabase
         # if you want email confirmation code needs to be added below
@@ -59,13 +48,10 @@ def register(request: Request, email: str = Form(...), password: str = Form(...)
             "email": email,
             "password": pwd
         })
-        print(response.session.access_token)
-
-        # redirect to the login page
     except AuthApiError as e:
         if "User already registered" in str(e):
             print("User already registered")
-        # return registration page with error
+        # return registration form page with error
 
 @router.post("/login", response_class=HTMLResponse)
 def login(request: Request, email: str = Form(...), password: str = Form(...)):
